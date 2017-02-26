078df5c9baaff730a62090abf715d2d57ea04079
Tyler Stapler <tystapler@gmail.com>
2017-02-25 21:16:50 -0600

    Move resume-editor to separate component

---
+++ client/src/app/resume-editor/index.ts
@@ -0,0 +1,1 @@
+export * from './resume-editor.component';
---
+++ client/src/app/resume-editor/resume-editor.component.ts
@@ -0,0 +1,57 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import { AppState } from '../app.service';
+
+import { resumeSchema } from './../resume_schema';
+import { resumeLayout } from './../resume_form_layout';
+
+import { FrameworkLibraryService } from 'angular2-json-schema-form';
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'resume-editor'
+  selector: 'resume-editor',  // <resume-editor></resume-editor>
+  // We need to tell Angular's Dependency Injection which providers are in our app.
+  // Our list of styles in our component. We may add more to compose many styles together
+  styleUrls: [ './resume-editor.component.css' ],
+  // Every Angular template is first compiled by the browser before Angular runs it's compiler
+  templateUrl: './resume-editor.component.html'
+})
+export class ResumeEditorComponent implements OnInit {
+  // Set our default values
+  public localState = { value: '' };
+  public mySchema = resumeSchema;
+  public appLayout = resumeLayout;
+  // TypeScript public modifiers
+  constructor(
+    public appState: AppState,
+  ) {
+  }
+
+  public ngOnInit() {
+    console.log('hello `ResumeEditor` component');
+    // this.title.getData().subscribe(data => this.data = data);
+  }
+
+  public submitState(value: string) {
+    console.log('submitState', value);
+    this.appState.set('value', value);
+    this.localState.value = '';
+  }
+
+  public submitThing(val) {
+    console.log(val);
+  }
+
+  public showFormSchemaFn(evnt) {
+    console.log(evnt);
+  }
+
+  public showFormLayoutFn(evnt) {
+    console.log(evnt);
+  }
+}
