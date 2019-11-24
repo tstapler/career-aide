0f2d29b63a2e1b44f824939d54afb16a26388053
Tyler Stapler <tystapler@gmail.com>
2019-11-23 18:14:07 -0600

    Update with support for more resumes and format

--- common/models/resume.js
+++ common/models/resume.js
@@ -1,55 +1,55 @@
-'use strict';
+'use strict'
 
-const {exec} = require('child_process');
-const path = require('path');
-const fs = require('fs-extra');
-const {supportedThemes} = require('../themes');
+const {exec} = require('child_process')
+const path = require('path')
+const fs = require('fs-extra')
+const {supportedThemes} = require('../themes')
 const _ = require('lodash')
 
 const contentTypes = {
   pdf: 'application/pdf',
   html: 'text/html',
-};
+}
 
-const TMP_FOLDER = 'generated-files';
+const TMP_FOLDER = 'generated-files'
 
 function generateFile(userId, resumeId, themeName,
   resume, fileFormat, callback) {
   if (!resume) {
-    console.log('No resume!');
-    return callback(true, '', 'text/html', '');
+    console.log('No resume!')
+    return callback(true, '', 'text/html', '')
   }
 
 
   if (!_.includes(supportedThemes, themeName)) {
-    console.log('Unsupported Resume name');
-    return callback(true, '', 'text/html', '');
+    console.log('Unsupported Resume name')
+    return callback(true, '', 'text/html', '')
   }
 
-  let filename = `u_${userId}-r_${resumeId}`;
-  let jsonFile = path.join(TMP_FOLDER, `${filename}.json`);
-  let outputFile = path.join(TMP_FOLDER, `${filename}_${themeName}.${fileFormat}`);
-  let themeModule = path.join('node_modules', `jsonresume-theme-${themeName}`);
+  let filename = `u_${userId}-r_${resumeId}`
+  let jsonFile = path.join(TMP_FOLDER, `${filename}.json`)
+  let outputFile = path.join(TMP_FOLDER, `${filename}_${themeName}.${fileFormat}`)
+  let themeModule = path.join('node_modules', `jsonresume-theme-${themeName}`)
 
-  fs.ensureDir(TMP_FOLDER, err => console.error(err));
+  fs.ensureDir(TMP_FOLDER, err => console.error(err))
 
   fs.writeJson(jsonFile, resume.data).then(() => {
-    console.log('Created shit');
+    console.log('Created shit')
     let hmrExec = `node_modules/.bin/hackmyresume BUILD ${jsonFile} TO ${outputFile} -t ${themeModule} -p weasyprint`
-    console.log(hmrExec);
+    console.log(hmrExec)
     exec(hmrExec,
       () => {
-        let contentType = contentTypes[fileFormat];
-        let contentDisposition = `filename=${filename}`;
+        let contentType = contentTypes[fileFormat]
+        let contentDisposition = `filename=${filename}`
         fs.readFile(outputFile, (err, fileData) => {
           if (err) {
-            console.error(err);
+            console.error(err)
           }
-          callback(null, fileData, contentType, contentDisposition);
+          callback(null, fileData, contentType, contentDisposition)
           // TODO: Clean up after
-        });
-      });
-  });
+        })
+      })
+  })
 }
 
 module.exports = (Resume) => {
@@ -61,9 +61,9 @@ module.exports = (Resume) => {
       },
     }, (err, resume) => {
       generateFile(userId, resumeId, themeName,
-        resume, 'pdf', callback);
-    });
-  };
+        resume, 'pdf', callback)
+    })
+  }
 
   Resume.generateHtml = (userId, resumeId, themeName, callback) => {
     Resume.findOne({
@@ -73,7 +73,14 @@ module.exports = (Resume) => {
       },
     }, (err, resume) => {
       generateFile(userId, resumeId, themeName,
-        resume, 'html', callback);
-    });
-  };
-};
+        resume, 'html', callback)
+    })
+  }
+
+// remote method after hook
+  Resume.afterRemote('**', function(ctx, remoteMethodOutput, next) {
+    console.log('Setting xframe options')
+    ctx.res.set('X-Frame-Options', 'sameorigin')
+    next()
+  })
+}
